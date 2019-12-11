aaf3c2507dd0a26ce3c0565cbe601f7f3aaf2c95
Pragya <prog187.pg@gmail.com>
2019-12-11 18:41:24 -0500

    Initial Commit

---
+++ BMIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIApp
+//
+//  Created by Student on 2019-12-11.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
